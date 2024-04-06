6379484d6b4fff964007840c4b48281d6c089743
kao-chinaklp <71183596+kao-chinaklp@users.noreply.github.com>
2024-04-06 13:39:41 +0800

    up

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
-window.onload = function() {
-}
+$(window).scroll(function() {
+    var scrollPos = $(this).scrollTop();
+    var windowHeight = $(window).height();
+
+    $('.p-word').each(function() {
+        var wordPos = $(this).offset().top;
+
+        if (scrollPos + windowHeight > wordPos) {
+            $(this).addClass('fadein-both');
+        }
+    });
+});
