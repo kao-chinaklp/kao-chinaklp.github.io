8c4e80b4c230440d54e41b2834100469d57d1ca3
kao-chinaklp <71183596+kao-chinaklp@users.noreply.github.com>
2024-04-13 23:01:02 +0800

    up

--- js/index.js
+++ js/index.js
@@ -1,12 +1,19 @@
 $(window).scroll(function() {
     var scrollPos = $(document).scrollTop();
     var windowHeight = $(window).height();
-
-    $('.p-word').each(function() {
-        var wordPos = $(this).offset().top;
-
-        if (scrollPos + windowHeight > wordPos) {
-            $(this).addClass('fadeinboth');
-        }
-    });
+    
+    var wordPos = $('.text1').offset().top;
+    if (scrollPos + windowHeight> wordPos) {
+        $('.text1').addClass('slideinfrombottom');
+    }
+    
+    wordPos = $('.text2').offset().top;
+    if (scrollPos + windowHeight / 2 > wordPos) {
+        $('.text2').addClass('slideinfromright');
+    }
+    
+    wordPos = $('.text3').offset().top;
+    if (scrollPos + windowHeight / 2 > wordPos) {
+        $('.text3').addClass('slideinfromleft');
+    }
 });
